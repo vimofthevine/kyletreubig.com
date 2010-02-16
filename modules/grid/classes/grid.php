b7f9de3cf7328c8cf3c0f5e3ab9273ab1f39a0a4
vimofthevine <kyletreubig@gmail.com>
2010-02-15 20:32:10 -0500

    Linking to grid module

--- modules/grid/classes/grid.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
-/**
- * @brief   Grid modeling library for creating data tables
- * @author  Kyle Treubig
- */
-class Grid extends Grid_Core { }
-
