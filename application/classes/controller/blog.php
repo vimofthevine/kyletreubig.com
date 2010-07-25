5d6d7ff4cd5418b9e6ac9750588c9e61989f43ac
vimofthevine <kyletreubig@gmail.com>
2010-07-25 14:56:49 -0400

    Adjusted for slight change in blog module api

--- application/classes/controller/blog.php
+++ application/classes/controller/blog.php
@@ -7,13 +7,13 @@ class Controller_Blog extends Controller_Blog_Articles {
 	 */
 	public function after() {
 		$recent_articles = Request::factory( Route::get('blog/stats')
-			->uri(array('action'=>'recent_articles', 'id'=>3)))
+			->uri(array('action'=>'recent_articles', 'limit'=>3)))
 			->execute()->response;
 		$popular_articles = Request::factory( Route::get('blog/stats')
-			->uri(array('action'=>'popular_articles', 'id'=>3)))
+			->uri(array('action'=>'popular_articles', 'limit'=>3)))
 			->execute()->response;
 		$recent_comments = Request::factory( Route::get('blog/stats')
-			->uri(array('action'=>'recent_comments', 'id'=>3)))
+			->uri(array('action'=>'recent_comments', 'limit'=>3)))
 			->execute()->response;
 
 		$side = isset($this->template->side) ? $this->template->side : NULL;
